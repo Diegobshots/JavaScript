2dc254a619c25f766dae1c650b1fd31b9b956abe
diegobshots <blancolopez94@gmail.com>
2019-09-24 12:37:47 +0200

    24/09

--- Botones/bucles.js
+++ Botones/bucles.js
@@ -1,3 +1,43 @@
-for ( var i=0; i<11 ; i++){
-    console.log(i);
+//Asignacion de elementos DOM
+var botonFor = document.getElementById("botonFor");
+var botonWhile = document.getElementById("botonWhile");
+var botonDoWhile = document.getElementById("botonDoWhile");
+var botonCalcularMedia = document.getElementById("botonCalcularMedia");
+
+//Eventos
+botonFor.addEventListener("click", fBucleFor);
+botonWhile.addEventListener("click", fBucleWhile);
+botonDoWhile.addEventListener("click", fBucleDoWhile);
+botonCalcularMedia.addEventListener("click", fCalcularMedia);
+
+//Funciones
+function fBucleFor(){
+    for (var i=1; i<11; i++){
+        console.log(i);  
+    }
+}
+function fBucleWhile(){
+    var x=1;
+    while(x<11){
+        console.log(x);
+        x++;
+    }
+}
+
+function fBucleDoWhile(){
+    var y=1;
+    do{
+        console.log(y);
+        y++;
+    }while(y<11);
+}
+
+function fCalcularMedia(){
+    var total=0;
+    for (var i=1; i<11; i++){
+        
+        var numero = parseInt(prompt("Introduce el numero ".i));
+        total += numero;
+    }
+    console.log(total/10);
 }

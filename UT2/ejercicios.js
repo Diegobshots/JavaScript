f34289b77b341480382a4f60a144e5adf20a5617
diegobshots <blancolopez94@gmail.com>
2019-09-25 14:23:27 +0200

    25/09

---
+++ UT2/ejercicios.js
@@ -0,0 +1,42 @@
+//Botones
+var boton1 = document.getElementById("e1");
+var boton2 = document.getElementById("e2");
+
+//Eventos
+boton1.addEventListener("click",fEjercicio1);
+boton2.addEventListener("click",fEjercicio2);
+
+//Funciones
+function fEjercicio1() {
+    
+    var edad =0;
+    edad = prompt("Introduce tu edad: ");
+    if (edad <18){
+        alert("No puedes conducir");
+    }else{
+        alert("Puedes conducir");
+    }
+}
+function fEjercicio2(){
+    var nota = parseInt(prompt("Introduce tu nota: "));
+    switch(nota){
+        case 0, 1, 2:
+            alert("Muy deficiente");
+            break;
+        case 3, 4:
+            alert("Deficiente");
+            break;
+        case 5, 6:
+            alert("Bien");
+            break;
+        case 7, 8:
+            alert("Notable");
+            break;
+        case 9, 10:
+            alert("Sobresaliente");
+            break;
+        default:
+            alert("Solo parametros entre 0 y 10");
+            break;
+    }
+}

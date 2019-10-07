57f0eb00ebbe5dfc6cb0bc81d37cc27108eef727
diegobshots <blancolopez94@gmail.com>
2019-10-07 13:12:17 +0200

    7/10 clase

--- UT2/Ejercicios Básicos/hoja1.js
+++ UT2/Ejercicios Básicos/hoja1.js
@@ -1,12 +1,13 @@
 //Botones
 var boton1 = document.getElementById("ejercicio3");
 var boton2 = document.getElementById("ejercicio4");
+var boton4 = document.getElementById("ejercicio5");
 
 
 //Eventos
 boton1.addEventListener("click", fEjercicio3);
 boton2.addEventListener("click", fEjercicio4);
-
+boton4.addEventListener("click", fEjercicio5);
 
 //Funciones
 function fEjercicio3() {
@@ -34,4 +35,41 @@ function fEjercicio4() {
         }
     }
     alert("El total de numeros es: "+numeroSumado);
+}
+
+function fEjercicio5() {
+    var dni = 0;
+    while(dni >=0 && dni<=999999999){
+        dni = prompt("Introduce tu DNI sin letra: ");
+        if(isNaN(dni)){
+            alert("Introduce solo numeros");
+        }else{
+            parseInt(dni);
+            resto = dni%23;
+            switch(resto){
+                case 0:
+                    alert("Letra: T");
+                    break;
+                case 1:
+                    alert("Letra: R");
+                    break;
+                case 2:
+                    alert("Letra: W");
+                    break;
+                case 3:
+                    alert("Letra: A");
+                    break;
+                case 4:
+                    alert("Letra: G");
+                    break;
+                case 5:
+                    alert("Letra: M");
+                    break;
+                case 6:
+                    alert("Letra: Y");
+                    break;
+                //Y ya paro porque esto se puede hacer con un array y me aburro hehe
+            }
+        }
+    }
 }

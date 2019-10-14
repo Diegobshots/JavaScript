136aa67cd1f8490c12e17d8bf9e2130accf733d1
diegobshots <blancolopez94@gmail.com>
2019-10-14 14:16:35 +0200

    14/10 clase

--- Pruebas Sueltas/pruebaWindow/1.js
+++ Pruebas Sueltas/pruebaWindow/1.js
@@ -1,6 +1,12 @@
 var boton1 = window.document.getElementById("boton");
+var boton2 = window.document.getElementById("botonMetodos")
+var boton3 = window.document.getElementById("botonPropiedades")
+var listaPropiedades= "<h1> Propiedades </h1> ";
+listaMetodos = "<h1> Metodos </h1> ";
 
 boton1.addEventListener("click", fBoton);
+boton2.addEventListener("click", fBotonMetodos);
+boton3.addEventListener("click", fBotonPropiedades);
 
 function fBoton() {
     var valores = window.document.getElementsByTagName("input");
@@ -8,4 +14,20 @@ function fBoton() {
         console.log(valores[i].value)
     }
     
+}
+
+function fBotonMetodos() {
+    var metodos = Object.keys(window);
+    for (let i=0 ; i<=metodos.length ; i++){
+        listaMetodos += metodos[i]+"  <br/> ";
+    }
+    document.getElementById("metodos").innerHTML = listaMetodos;
+}
+function fBotonPropiedades() {
+    var propiedades = Object.getOwnPropertyNames(window);
+    for (let i=0 ; i<=propiedades.length ; i++){
+        listaPropiedades += propiedades[i]+" <br/> ";
+    }
+    document.getElementById("propiedades").innerHTML = listaPropiedades;
+    //Se borra todo cuando acaba
 }

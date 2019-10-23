6f5cbfcb79d0aac4dec8c6a2bc543e6ec07f670e
diegobshots <blancolopez94@gmail.com>
2019-10-23 14:20:39 +0200

    23/10 clase

--- UT3/EjerciciosPDF/3/3.js
+++ UT3/EjerciciosPDF/3/3.js
@@ -14,9 +14,10 @@ function fBoton(){
     texto = texto.toLowerCase();
     arrayTexto = texto.split("");
     var quijote = document.getElementById("quijote").innerText;
+    quijote = quijote.toLowerCase();
     arrayQuijote = quijote.split("");
     apariciones = 0;
-    for (let i; i<arrayQuijote.lenght ; i++){
+    for (let i=0; i<arrayQuijote.length ; i++){
         if (arrayTexto[0] === arrayQuijote[i]){
             apariciones++;
         }
---
+++ UT3/EjerciciosPDF/4/4.js
@@ -0,0 +1,10 @@
+//Elementos
+
+var boton = document.getElementById("boton");
+
+//Eventos
+
+boton.addEventListener("click", fBoton);
+
+
+//funciones

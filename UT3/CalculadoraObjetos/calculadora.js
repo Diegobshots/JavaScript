d7819384e571ac6ec4bf829e52e6bcf69822faa7
diegobshots <blancolopez4@gmail.com>
2019-11-18 23:03:59 +0100

    18/11 clase

--- UT3/CalculadoraObjetos/calculadora.js
+++ UT3/CalculadoraObjetos/calculadora.js
@@ -1,4 +1,6 @@
+//Aqui captamos cada uno de los botones
 let pantalla = document.getElementById("pantalla");
+let pantallaOper = document.getElementById("pantallaOper");
 let uno = document.getElementById("uno");
 let dos = document.getElementById("dos");
 let tres = document.getElementById("tres");
@@ -13,23 +15,79 @@ let igual = document.getElementById("igual");
 let mas = document.getElementById("mas");
 let menos = document.getElementById("menos");
 let reset = document.getElementById("reset");
+let operador = "";
 
+//Creamos el objeto con el que vamos a trabajar
 var calculadora ={
     numero1:0,
     numero2:0,
     sumar : function sumaNumeros(){
-        return this.numero1+this.numero2;
+        return parseInt(this.numero1)+parseInt(this.numero2);
+    },
+    restar : function sumaNumeros(){
+        
+        return parseInt(this.numero1)-parseInt(this.numero2);
     }
 };
-calculadora.numero1= 9;
-calculadora.numero2= 1;
 
-console.log(calculadora.sumar());
-let fBotonUno = () => pantalla.innerHTML += 5;
+//Cada boton tiene una funcion que modifica el objeto o llama a un método del objeto 
+let fBotonUno = () => pantalla.innerHTML += 1;
 uno.addEventListener("click", fBotonUno);
 
 let fBotonDos = () => pantalla.innerHTML += 2;
-uno.addEventListener("click", fBotonDos);
+dos.addEventListener("click", fBotonDos);
 
-let fBotonMas = () => num1 = num1;
-uno.addEventListener("click", fBotonUno);
+let fBotonTres = () => pantalla.innerHTML += 3;
+tres.addEventListener("click", fBotonTres);
+
+let fBotonCuatro = () => pantalla.innerHTML += 4;
+cuatro.addEventListener("click", fBotonCuatro);
+
+let fBotonCinco = () => pantalla.innerHTML += 5;
+cinco.addEventListener("click", fBotonCinco);
+
+let fBotonSeis = () => pantalla.innerHTML += 6;
+seis.addEventListener("click", fBotonSeis);
+
+let fBotonSiete = () => pantalla.innerHTML += 7;
+siete.addEventListener("click", fBotonSiete);
+
+let fBotonOcho = () => pantalla.innerHTML += 8;
+ocho.addEventListener("click", fBotonOcho);
+
+let fBotonNueve = () => pantalla.innerHTML += 9;
+nueve.addEventListener("click", fBotonNueve);
+
+let fBotonCero = () => pantalla.innerHTML += 0;
+cero.addEventListener("click", fBotonCero);
+
+let fBotonMas = () =>{
+    calculadora.numero1 = pantalla.innerHTML;
+    pantalla.innerHTML = "";
+    pantallaOper.innerHTML = "+";
+    operador = "+";
+} 
+mas.addEventListener("click", fBotonMas);
+
+let fBotonMenos = () =>{
+    calculadora.numero1 = pantalla.innerHTML;
+    pantalla.innerHTML = "";
+    pantallaOper.innerHTML = "-";
+    operador = "-";
+    
+} 
+menos.addEventListener("click", fBotonMenos);
+
+let fBotonIgual = () =>{
+    pantallaOper.innerHTML = "";
+    calculadora.numero2 = pantalla.innerHTML;
+    switch (operador){
+        case "+":
+            pantalla.innerHTML = calculadora.sumar();
+            break;
+        case "-":
+            pantalla.innerHTML = calculadora.restar();
+            break;
+    }
+} 
+igual.addEventListener("click", fBotonIgual);

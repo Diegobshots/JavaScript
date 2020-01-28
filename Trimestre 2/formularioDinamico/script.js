ab2257a8ebc0f516f348f77d1aa631dbb4d24a58
diegobshots <blancolopez94@gmail.com>
2020-01-28 13:55:52 +0100

    28/01 clase

---
+++ Trimestre 2/formularioDinamico/script.js
@@ -0,0 +1,20 @@
+var contenedor = document.getElementById("contenedor");
+
+
+class Formulario{
+    constructor(idForm){
+        this.idForm = idForm
+        this.elementos = []
+    }
+    añadirElemento(...Elemento){
+        this.elementos.push(Elemento)
+        return this.conocimientos
+    }
+    pintarFormulario(){
+        //concatena cabecera del formulario
+        //bucle recorre vector elementos y los concatena en un array
+        //pie del formulario
+    }
+}
+
+let formulario1 = new Formulario(1)

88b994d5ae094110f385e7b87bca71c1c0412f81
diegobshots <blancolopez4@gmail.com>
2020-02-04 17:21:41 +0100

    04/02 casa

--- Trimestre 2/JSON/Diego/script.js
+++ Trimestre 2/JSON/Diego/script.js
@@ -1,14 +1,22 @@
 
+// Obtener la instancia del objeto XMLHttpRequest
+xmlhttp = new XMLHttpRequest();
 
 
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var respuesta = JSON.parse(xhttp.responseText);
-      var cv = respuesta.cv;
-      var salida = '';
-      console.log(cv.nombre);
-    }
-  };
-  xhttp.open("GET", "cv.json", true);
-  xhttp.send();
+// Preparar la funcion de respuesta
+xmlhttp.onreadystatechange = function () {
+  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    var jsonResponse = xmlhttp.responseText;
+    var objeto_json = JSON.parse(jsonResponse);
+
+    document.getElementById("fname").value = objeto_json.nombre;
+    document.getElementById("lname").value = objeto_json.apellidos;
+    document.getElementById("email").value = objeto_json.correo;
+    document.getElementById("telefono").value = objeto_json.telefono;
+    
+  }
+}
+
+//xmlhttp.open("GET","listadoPaises.json?nocache=' + (new Date()).getTime()");
+xmlhttp.open("GET", "cv.json");
+xmlhttp.send();

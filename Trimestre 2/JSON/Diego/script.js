b6182acee9aba954f77a0a791aa7fc885ed854ce
diegobshots <blancolopez94@gmail.com>
2020-02-04 13:12:34 +0100

    04/02 clase

--- Trimestre 2/JSON/Diego/script.js
+++ Trimestre 2/JSON/Diego/script.js
@@ -1,22 +1,14 @@
 
 
 
-  /**
-   * var container = document.getElementById("container");
-
-var requestURL = 'cv.json';
-
-var request = new XMLHttpRequest();
-request.open(document, requestURL);
-
-request.responseType = 'json';
-request.send();
-
-request.onload = function() {
-    var curriculum = request.response;
-    console.log(curriculum);
-  }
-   */
-
-  var json = require('cv.json');
-  console.log(json);
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var respuesta = JSON.parse(xhttp.responseText);
+      var cv = respuesta.cv;
+      var salida = '';
+      console.log(cv.nombre);
+    }
+  };
+  xhttp.open("GET", "cv.json", true);
+  xhttp.send();

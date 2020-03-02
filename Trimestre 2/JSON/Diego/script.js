85ecf1b1d969bc6740cd58d4e5b77ca4035fe7b3
diegobshots <blancolopez94@gmail.com>
2020-03-02 14:21:41 +0100

    2/3 clase

--- Trimestre 2/JSON/Diego/script.js
+++ Trimestre 2/JSON/Diego/script.js
@@ -1,34 +1,40 @@
+var btn = document.getElementById("botonaco");
 
-cargarDatos(muestraDescripciones());
+var app = {
+  conexion: function cenexion() {
 
-
-
-function muestraDescripciones(){
-  
-}
-function cargarDatos() {
-  // Obtener la instancia del objeto XMLHttpRequest
-  xmlhttp = new XMLHttpRequest();
+    // Obtener la instancia del objeto XMLHttpRequest
+    xmlhttp = new XMLHttpRequest();
 
 
-  // Preparar la funcion de respuesta
-  xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      var jsonResponse = xmlhttp.responseText;
-      var objeto_json = JSON.parse(jsonResponse);
+    // Preparar la funcion de respuesta
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var jsonResponse = xmlhttp.responseText;
+        var objeto_json = JSON.parse(jsonResponse);
 
-      document.getElementById("fname").value = objeto_json.nombre;
-      document.getElementById("lname").value = objeto_json.apellidos;
-      document.getElementById("email").value = objeto_json.correo;
-      document.getElementById("telefono").value = objeto_json.telefono;
-      document.getElementById("idioma1").value = objeto_json.idiomas[0].nombre;
-      document.getElementById("idioma2").value = objeto_json.idiomas[1].nombre;
+        document.getElementById("fname").value = objeto_json.nombre;
+        document.getElementById("lname").value = objeto_json.apellidos;
+        document.getElementById("email").value = objeto_json.correo;
+        document.getElementById("telefono").value = objeto_json.telefono;
+        document.getElementById("idioma1").value = objeto_json.idiomas[0].nombre;
+        document.getElementById("idioma2").value = objeto_json.idiomas[1].nombre;
+        document.getElementById("estudio1").value = objeto_json.estudios[0].titulo;
+        document.getElementById("estudio2").value = objeto_json.estudios[1].titulo;
+        document.getElementById("centro1").value = objeto_json.estudios[0].centro;
+        document.getElementById("centro2").value = objeto_json.estudios[1].centro;
+        document.getElementById("anio1").value = objeto_json.estudios[0].anioFin;
+        document.getElementById("anio2").value = objeto_json.estudios[1].anioFin;
+
+
+      }
+
     }
-    callback();
 
+    xmlhttp.open("GET", "cv.json", true);
+    xmlhttp.send();
   }
 
-  //xmlhttp.open("GET","listadoPaises.json?nocache=' + (new Date()).getTime()");
-  xmlhttp.open("GET", "cv.json");
-  xmlhttp.send();
 }
+
+window.onload = app.conexion;

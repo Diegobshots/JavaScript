69e983cda216cb045c48050510d8ee838489affa
diegobshots <blancolopez94@gmail.com>
2020-03-03 12:46:05 +0100

    3/3 clase

--- Trimestre 2/JSON/Diego/script2.js
+++ Trimestre 2/JSON/Diego/script2.js
@@ -1,31 +1,40 @@
 let boton = document.getElementById("botonaco");
 let app = {
-    contratar: function contratar(){
+    contratar: function contratar() {
         document.body.style.backgroundImage = "url('./img/background.gif')";
         boton.style.backgroundColor = "green";
         boton.innerHTML = "¡Contratado!";
         document.getElementById("fotoCV").style.border = "3px solid #00ff0d";
         document.getElementById("fotoCV").style.animation = "rebota 0.3s infinite";
-      },
-    rellenaCV : function rellena(){
+    },
+    rellenaCV: function rellena() {
         fetch('cv.json')
-        .then(data => data.json())
-        .then(users => {
-            document.getElementById("fname").value = users.nombre;
-            document.getElementById("titulo").innerHTML = users.nombre;
-            document.getElementById("lname").value = users.apellidos;
-            document.getElementById("email").value = users.correo;
-            document.getElementById("telefono").value = users.telefono;
-            document.getElementById("idioma1").value = users.idiomas[0].nombre;
-            document.getElementById("idioma2").value = users.idiomas[1].nombre;
-            document.getElementById("estudio1").value = users.estudios[0].titulo;
-            document.getElementById("estudio2").value = users.estudios[1].titulo;
-            document.getElementById("centro1").value = users.estudios[0].centro;
-            document.getElementById("centro2").value = users.estudios[1].centro;
-            document.getElementById("anio1").value = users.estudios[0].anioFin;
-            document.getElementById("anio2").value = users.estudios[1].anioFin;
-            document.getElementById("fotoCV").src = users.foto;
-        });
+            .then(data => data.json())
+            .then(contenidoJson => {
+                //Cargamos datos personales
+                document.getElementById("fotoCV").src = contenidoJson.datosPersonales[0].foto;
+                document.getElementById("fname").value = contenidoJson.datosPersonales[1].nombre;
+                document.getElementById("titulo").innerHTML = contenidoJson.datosPersonales[1].nombre;
+                document.getElementById("lname").value = contenidoJson.datosPersonales[2].apellidos;
+                document.getElementById("email").value = contenidoJson.datosPersonales[3].correo;
+                document.getElementById("telefono").value = contenidoJson.telefono;
+                return contenidoJson;
+            })
+            .then(contenidoJson => {
+                //Cargamos idiomas
+                document.getElementById("idioma1").value = contenidoJson.idiomas[0].nombre;
+                document.getElementById("idioma2").value = contenidoJson.idiomas[1].nombre;
+                return contenidoJson;
+            })
+            .then(contenidoJson => {
+                //Cargamos estudios
+                document.getElementById("estudio1").value = contenidoJson.estudios[0].titulo;
+                document.getElementById("estudio2").value = contenidoJson.estudios[1].titulo;
+                document.getElementById("centro1").value = contenidoJson.estudios[0].centro;
+                document.getElementById("centro2").value = contenidoJson.estudios[1].centro;
+                document.getElementById("anio1").value = contenidoJson.estudios[0].anioFin;
+                document.getElementById("anio2").value = contenidoJson.estudios[1].anioFin;
+            });
     }
 }
 
